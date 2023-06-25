858d91f92dde3119994b3093327cbdd6a1d97410
Pytholearn <109828387+Pytholearn@users.noreply.github.com>
2023-06-25 15:50:07 +0330

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,10 @@
+#discord: https://discord.gg/xbN3XSHYjx
+#youtube: https://www.youtube.com/@IIIHaZaRd
+#pytholearn
+import pyautogui
+pic = pyautogui.screenshot()
+pic.save("H:\\python project\\screenshot\\picc.jpg")
+
+#discord: https://discord.gg/xbN3XSHYjx
+#youtube: https://www.youtube.com/@IIIHaZaRd
+#pytholearn
